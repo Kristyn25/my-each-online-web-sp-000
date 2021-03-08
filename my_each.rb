597fdb666684a6a837559bd1b5fe7394c13a7bef
Kristyn Little <kristynlittle@frontier.com>
2021-03-08 03:48:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+dogs = ["Jack Russell", "Golden Retriever", "Newfoundland", "Boxer"]
+my_each(dogs) do |i|
+  puts i 
+end
+
+#def my_each(dogs)# put argument(s) here
+  
   # code here
 end
